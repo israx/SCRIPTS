dca390eefa7fce385768c96e375bc5a0cf91c3c5
Israel Arcos <israel.arcos2595@gmail.com>
2025-06-17 15:21:45 -0700

    chore: delete unused files

--- dynamo/dist/dynamodb-client.d.ts
+++
@@ -1,6 +0,0 @@
-import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
-import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
-declare const dynamoDBClient: DynamoDBClient;
-export declare const docClient: DynamoDBDocumentClient;
-export { dynamoDBClient };
-//# sourceMappingURL=dynamodb-client.d.ts.map
